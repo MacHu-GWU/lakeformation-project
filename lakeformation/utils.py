0a38fd22b8c8cd47e4c94a13807bff378a5bf286
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-04-03 19:44:58 -0400

    update utils

--- lakeformation/utils.py
+++ lakeformation/utils.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from typing import List, Set, Tuple, Dict, Iterable, Any
+from datetime import datetime, timezone
+
 var_name_escape = {
     "-": "_",
     ":": "_",
@@ -24,3 +27,70 @@ def to_var_name(s: str) -> str:
     for k, v in var_name_escape.items():
         s = s.replace(k, v)
     return s
+
+
+def get_local_and_utc_now() -> Tuple[datetime, datetime]:
+    """
+    Get current time in both local timezone format and utc format.
+    """
+    local_now = datetime.now().replace(microsecond=0)
+    local_tz = local_now.astimezone().tzinfo
+    local_now = local_now.replace(tzinfo=local_tz)
+    utc_now = local_now.astimezone(timezone.utc)
+    return local_now, utc_now
+
+
+def get_diff_and_inter(
+    dct1: Dict[str, Any],
+    dct2: Dict[str, Any],
+) -> Tuple[Set[str], Set[str], Set[str]]:
+    """
+    Each deployed ``Object`` usually has a unique id. ``Mapper`` is a dictionary
+    data structure that stores a collection of ``Object``. Key is the
+    ``Object`` id, value is the ``Object`` instance.
+
+    Given a new ``Mapper`` and a deployed ``Mapper``, it is very common to
+    find out which ``Object`` should be added, should be delayed and should be
+    updated.
+
+    This utility function takes two parameter
+
+    1. new object ``Mapper``
+    2. deployed object ``Mapper``
+
+    Returns three set data structure
+
+    1. to add object id set
+    2. to delete object id set
+    3. to update object id set
+
+    :param dct1:
+    :param dct2:
+    :return:
+    """
+    s1 = set(dct1)
+    s2 = set(dct2)
+    return s1.difference(s2), s2.difference(s1), s1.intersection(s2)
+
+
+def grouper_list(iterable: Iterable, n: int) -> List[list]:
+    """
+    Evenly divide list into fixed-length piece, no filled value if chunk
+    size smaller than fixed-length.
+
+    Example::
+
+        >>> list(grouper_list(range(10), n=3)
+        [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9]]
+    """
+    chunk = list()
+    counter = 0
+    for item in iterable:
+        counter += 1
+        chunk.append(item)
+        if counter == n:
+            yield chunk
+            chunk = list()
+            counter = 0
+    if len(chunk) > 0:
+        yield chunk
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import pytest
-from lakeformation.utils import to_var_name
+from datetime import timezone
+from lakeformation.utils import (
+    to_var_name,
+    get_local_and_utc_now,
+    get_diff_and_inter,
+    grouper_list
+)
 
 
 def test_to_var_name():
@@ -13,6 +19,26 @@ def test_to_var_name():
     ]
     for before, after in test_cases:
         assert to_var_name(before) == after
+
+
+def test_get_local_and_utc_now():
+    local_now, utc_now = get_local_and_utc_now()
+    assert local_now == utc_now
+    assert utc_now.tzinfo == timezone.utc
+
+
+def test_get_diff_and_inter():
+    d1 = dict(a=1, b=2)
+    d2 = dict(b=2, c=3)
+    s1, s2, s3 = get_diff_and_inter(d1, d2)
+    assert s1 == {"a", }
+    assert s2 == {"c", }
+    assert s3 == {"b", }
+
+
+def test_grouper_list():
+    l = [1, 2, 3, 4, 5, 6, 7]
+    assert list(grouper_list(l, 3)) == [[1, 2, 3], [4, 5, 6], [7, ]]
 
 
 if __name__ == "__main__":
