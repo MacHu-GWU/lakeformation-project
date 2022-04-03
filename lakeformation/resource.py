ff520b3138c3876ffadf20cbce991fd9bc419aca
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-04-03 19:25:08 -0400

    add resource module

---
+++ lakeformation/resource.py
@@ -0,0 +1,211 @@
+# -*- coding: utf-8 -*-
+
+from typing import (
+    List, Tuple, Set, Dict, Iterable, Sequence, Mapping,
+    Union, Any, Optional, Type, TYPE_CHECKING
+)
+from box import Box
+
+from .abstract import HashableAbc, RenderableAbc, SerializableAbc
+from .constant import DELIMITER
+from .validator import validate_attr_type
+from .utils import to_var_name
+
+if TYPE_CHECKING:
+    pass
+
+
+class Resource(HashableAbc, RenderableAbc, SerializableAbc):
+    pass
+
+
+class DbTbCol(Resource):
+    @classmethod
+    def deserialize(cls, data: dict) -> Union['Database', 'Table', 'Column']:
+        if "account_id" in data:
+            return Database.deserialize(data)
+        elif "database" in data:
+            return Table.deserialize(data)
+        elif "table" in data:
+            return Column.deserialize(data)
+        else:
+            raise ValueError("it is not a 'Resource' deserializable dict!")
+
+    @property
+    def get_add_remove_lf_tags_arg_value(self) -> dict:
+        raise NotImplementedError
+
+
+class Database(Resource):
+    def __init__(
+        self,
+        account_id: str,
+        region: str,
+        name: str,
+    ):
+        self.account_id = account_id
+        self.region = region
+        self.name = name
+        self.validate()
+
+        self.t: Dict[str, Table] = Box()
+
+    def validate(self):
+        validate_attr_type(self, "account_id", self.account_id, str)
+        validate_attr_type(self, "region", self.region, str)
+        validate_attr_type(self, "name", self.name, str)
+
+    @property
+    def attr_name(self) -> str:
+        return to_var_name(f"{self.account_id}_{self.region}_{self.name}")
+
+    @property
+    def var_name(self) -> str:
+        return f"db_{self.attr_name}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(account_id={self.account_id!r}, region={self.region!r}, name={self.name!r})"
+
+    @property
+    def id(self) -> str:
+        return self.var_name
+
+    def serialize(self) -> dict:
+        return dict(
+            account_id=self.account_id,
+            region=self.region,
+            name=self.name,
+        )
+
+    @classmethod
+    def deserialize(cls, data: dict) -> 'Database':
+        return cls(
+            account_id=data["account_id"],
+            region=data["region"],
+            name=data["name"],
+        )
+
+    @property
+    def get_add_remove_lf_tags_arg_name(self) -> str:
+        return "Database"
+
+    @property
+    def get_add_remove_lf_tags_arg_value(self) -> dict:
+        return dict(
+            CatalogId=self.account_id,
+            Name=self.name,
+        )
+
+
+class Table(Resource):
+
+    def __init__(
+        self,
+        name: str,
+        database: Database,
+    ):
+        self.name = name
+        self.database = database
+        self.validate()
+
+        self.c: Dict[str, Column] = Box()
+
+    def validate(self):
+        validate_attr_type(self, "name", self.name, str)
+        validate_attr_type(self, "database", self.database, Database)
+
+    @property
+    def attr_name(self) -> str:
+        return to_var_name(self.name)
+
+    @property
+    def var_name(self) -> str:
+        return f"tb_{self.database.attr_name}_{self.attr_name}"
+
+    def __repr__(self):
+        return f"Table(database={self.database!r}, name={self.name!r})"
+
+    @property
+    def id(self) -> str:
+        return self.var_name
+
+    def serialize(self) -> dict:
+        return dict(
+            name=self.name,
+            database=self.database.serialize(),
+        )
+
+    @classmethod
+    def deserialize(cls, data: dict) -> 'Table':
+        return cls(
+            name=data["name"],
+            database=Database.deserialize(data["database"]),
+        )
+
+    @property
+    def get_add_remove_lf_tags_arg_name(self) -> str:
+        return "Table"
+
+    @property
+    def get_add_remove_lf_tags_arg_value(self) -> dict:
+        return dict(
+            CatalogId=self.database.account_id,
+            DatabaseName=self.database.name,
+            Name=self.name,
+        )
+
+
+class Column(Resource):
+    def __init__(
+        self,
+        name: str,
+        table: Table,
+    ):
+        self.name = name
+        self.table = table
+        self.validate()
+
+    def validate(self):
+        validate_attr_type(self, "name", self.name, str)
+        validate_attr_type(self, "table", self.table, Table)
+
+    @property
+    def attr_name(self) -> str:
+        return to_var_name(self.name)
+
+    @property
+    def var_name(self) -> str:
+        return f"col_{self.table.database.attr_name}_{self.table.attr_name}_{self.attr_name}"
+
+    def __repr__(self):
+        return f'Column(table={self.table!r}, name={self.name!r})'
+
+    @property
+    def id(self) -> str:
+        return self.var_name
+
+    def serialize(self) -> dict:
+        return dict(
+            name=self.name,
+            table=self.table.serialize(),
+        )
+
+    @classmethod
+    def deserialize(cls, data: dict) -> 'Column':
+        return cls(
+            name=data["name"],
+            table=Table.deserialize(data["table"]),
+        )
+
+    @property
+    def get_add_remove_lf_tags_arg_name(self) -> str:
+        return "TableWithColumns"
+
+    @property
+    def get_add_remove_lf_tags_arg_value(self) -> dict:
+        return dict(
+            CatalogId=self.table.database.account_id,
+            DatabaseName=self.table.database.name,
+            Name=self.table.name,
+            ColumnNames=[self.name, ]
+        )
---
+++ tests/test_resource.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from lakeformation.resource import (
+    DbTbCol, Database, Table, Column
+)
+from lakeformation.tests import Objects
+
+obj = Objects()
+
+
+def test_hash():
+    _ = set(obj.resource_list)
+
+    obj1 = Objects()
+    obj2 = Objects()
+
+    for r1, r2 in zip(obj1.resource_list, obj2.resource_list):
+        assert r1 == r2
+        assert r1.id == r2.id
+        assert hash(r1) == hash(r2)
+
+    assert obj1.db_amz != obj1.tb_amz_user
+
+
+def test_render():
+    assert obj.db_amz.var_name == "db_111122223333_us_east_1_amz"
+    assert obj.tb_amz_user.var_name == "tb_111122223333_us_east_1_amz_user"
+    assert obj.col_amz_user_id.var_name == "col_111122223333_us_east_1_amz_user_id"
+
+    assert repr(obj.db_amz) == "Database(account_id='111122223333', region='us-east-1', name='amz')"
+    assert repr(
+        obj.tb_amz_user) == "Table(database=Database(account_id='111122223333', region='us-east-1', name='amz'), name='user')"
+    assert repr(
+        obj.col_amz_user_id) == "Column(table=Table(database=Database(account_id='111122223333', region='us-east-1', name='amz'), name='user'), name='id')"
+
+
+def test_seder():
+    assert Database.deserialize(obj.db_amz.serialize()) == obj.db_amz
+    assert Table.deserialize(obj.tb_amz_user.serialize()) == obj.tb_amz_user
+    assert Column.deserialize(obj.col_amz_user_id.serialize()) == obj.col_amz_user_id
+
+    for r in obj.resource_list:
+        assert DbTbCol.deserialize(r.serialize()) == r
+
+
+if __name__ == "__main__":
+    import os
+
+    basename = os.path.basename(__file__)
+    pytest.main([basename, "-s", "--tb=native"])
